aa462d3b1ca6cf96e8afabd2162ff5846b1a5371
gorushkin <gorushkin@users.noreply.github.com>
2026-06-25 06:55:39 +0300

    LED-078 refactor(errors): clean up legacy error types in user integration tests

--- packages/shared/src/types/types.ts
+++ packages/shared/src/types/types.ts
@@ -9,22 +9,6 @@ import {
   moneyAmountString,
 } from "../validation/baseValidations";
 
-export type ErrorResponse = {
-  error: string;
-  message: string;
-};
-
-export type ValidationError = {
-  error: true;
-  errors: [
-    {
-      code: string;
-      field: string;
-      message: string;
-    },
-  ];
-};
-
 export type CurrencyCode = z.infer<typeof currencyCode>;
 
 export type IsoDatetimeString = z.infer<typeof isoDatetime>;
