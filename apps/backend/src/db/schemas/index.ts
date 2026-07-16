736858eb8778f05dafb393077e848c291ce5c893
gorushkin <gorushkin@users.noreply.github.com>
2026-07-16 11:37:06 +0300

    LED-66 feat(commodities): add commodities schema and update exports

--- apps/backend/src/db/schemas/index.ts
+++ apps/backend/src/db/schemas/index.ts
@@ -4,3 +4,4 @@ export { operationsTable, operationsRelations } from './operations';
 export { transactionsTable, transactionsRelations } from './transactions';
 export { settingsTable } from './settings';
 export { usersTable } from './users';
+export { commoditiesTable } from './commodities';
